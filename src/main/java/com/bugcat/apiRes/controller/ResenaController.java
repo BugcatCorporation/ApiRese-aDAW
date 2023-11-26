761c862a71ad2e57d24af10720481fba993edc82
MadAlexO <aluyo_2012@hotmail.com>
2023-11-26 16:14:02 -0500

    logs hechos

--- src/main/java/com/bugcat/apiRes/controller/ResenaController.java
+++ src/main/java/com/bugcat/apiRes/controller/ResenaController.java
@@ -1,61 +1,78 @@
-
 package com.bugcat.apiRes.controller;
 
 import com.bugcat.apiRes.entity.ResenaValoracion;
 import com.bugcat.apiRes.service.ResenaService;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/resena")
-
-
+@Slf4j
 public class ResenaController {
+
     @Autowired
     private ResenaService resenaService;
+
     @GetMapping("findAll")
-    public ResponseEntity<List<ResenaValoracion>> findAll(){
-        return new ResponseEntity<>(resenaService
-                .findAll(),
-                HttpStatus.OK);
-    }
-    
-    @GetMapping("findByResenavalSK/{resenavalSK}")
-    public ResponseEntity<ResenaValoracion> findByResenavalSK(
-            @PathVariable String resenavalSK){
-        return new ResponseEntity<>(resenaService
-                .findByResenavalSK(resenavalSK),
-                HttpStatus.OK);
-    }
-    
-    @PostMapping("add")
-    public ResponseEntity<ResenaValoracion> add(
-            @RequestBody ResenaValoracion resenaValoracion){
-        return new ResponseEntity<>(resenaService
-                .add(resenaValoracion),
-                HttpStatus.CREATED);
-    }
-    
-    
-    @PutMapping("update")
-    public ResponseEntity<ResenaValoracion> update(@RequestBody ResenaValoracion resenaValoracion) {
-        return new ResponseEntity<>(resenaService.update(resenaValoracion), HttpStatus.OK);
+    public ResponseEntity<List<ResenaValoracion>> findAll() {
+        try {
+            log.info("Reseñas obtenidas.");
+            return new ResponseEntity<>(resenaService.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Error al obtener todas las reseñas.", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-     @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable Long id){
-        resenaService.delete(id);
+    @GetMapping("findByResenavalSK/{resenavalSK}")
+    public ResponseEntity<ResenaValoracion> findByResenavalSK(
+            @PathVariable String resenavalSK) {
+        try {
+            log.info("Reseña encontrada.", resenavalSK);
+            return new ResponseEntity<>(resenaService.findByResenavalSK(resenavalSK), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Error al buscar reseña por ResenavalSK: {}.", resenavalSK, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-    
-    
+
+    @PostMapping("add")
+    public ResponseEntity<ResenaValoracion> add(
+            @RequestBody ResenaValoracion resenaValoracion) {
+        try {
+            log.info("Reseña agregada.", resenaValoracion);
+            return new ResponseEntity<>(resenaService.add(resenaValoracion), HttpStatus.CREATED);
+        } catch (Exception e) {
+            log.error("Error al agregar nueva reseña: {}.", resenaValoracion, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<ResenaValoracion> update(@RequestBody ResenaValoracion resenaValoracion) {
+        try {
+            log.info("Reseña actualizada {}.", resenaValoracion);
+            return new ResponseEntity<>(resenaService.update(resenaValoracion), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Error al actualizar reseña: {}.", resenaValoracion, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        try {
+            log.info("Eliminando reseña con ID: {}.", id);
+            resenaService.delete(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            log.error("Error al eliminar reseña con ID: {}.", id, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/bugcat/apiRes/service/ResenaServiceImpl.java
+++ src/main/java/com/bugcat/apiRes/service/ResenaServiceImpl.java
@@ -1,48 +1,67 @@
-
 package com.bugcat.apiRes.service;
 
 import com.bugcat.apiRes.dao.ResenaRepository;
 import com.bugcat.apiRes.entity.ResenaValoracion;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ResenaServiceImpl implements ResenaService {
-    
+
     @Autowired
     private ResenaRepository resenaRepository;
 
     @Override
     public List<ResenaValoracion> findAll() {
-         return resenaRepository.findAll();
+        log.info("Obteniendo todas las reseñas.");
+        return resenaRepository.findAll();
     }
 
     @Override
     public ResenaValoracion findByResenavalSK(String resenavalSK) {
-      return resenaRepository.findByResenavalSK(resenavalSK);
+        log.info("Buscando reseña por ResenavalSK: {}.", resenavalSK);
+        return resenaRepository.findByResenavalSK(resenavalSK);
     }
 
     @Override
     public ResenaValoracion add(ResenaValoracion resenavaloracion) {
-      return resenaRepository.save(resenavaloracion);
+        log.info("Agregando nueva reseña: {}.", resenavaloracion);
+        return resenaRepository.save(resenavaloracion);
     }
 
     @Override
     public ResenaValoracion update(ResenaValoracion resenavaloracion) {
-      var resenaDB = resenaRepository.findById(resenavaloracion.getId()).get();
-        resenaDB.setComentario(resenavaloracion.getComentario());
-        resenaDB.setValoracion(resenavaloracion.getValoracion());
-         resenaDB.setFecha(resenavaloracion.getFecha());
-         resenaDB.setTitulo(resenavaloracion.getTitulo());
-          resenaDB.setResenavalSK(resenavaloracion.getResenavalSK());
-        return resenaRepository.save(resenaDB);
+        try {
+            log.info("Actualizando reseña: {}.", resenavaloracion);
+            var resenaDB = resenaRepository.findById(resenavaloracion.getId()).get();
+            resenaDB.setComentario(resenavaloracion.getComentario());
+            resenaDB.setValoracion(resenavaloracion.getValoracion());
+            resenaDB.setFecha(resenavaloracion.getFecha());
+            resenaDB.setTitulo(resenavaloracion.getTitulo());
+            resenaDB.setResenavalSK(resenavaloracion.getResenavalSK());
+            return resenaRepository.save(resenaDB);
+        } catch (Exception e) {
+            log.error("Error al actualizar reseña.", e);
+            throw e;
+        }
     }
 
     @Override
     public void delete(Long id) {
-      var resenaDB = resenaRepository.findById(id).get();
-        resenaRepository.delete(resenaDB);
+        try {
+            log.info("Eliminando reseña con ID: {}.", id);
+            var resenaDB = resenaRepository.findById(id).orElse(null);
+            if (resenaDB != null) {
+                resenaRepository.delete(resenaDB);
+            } else {
+                log.warn("No se encontró ninguna reseña con ID: {}.", id);
+            }
+        } catch (Exception e) {
+            log.error("Error al eliminar reseña.", e);
+            throw e;
+        }
     }
-    
 }
